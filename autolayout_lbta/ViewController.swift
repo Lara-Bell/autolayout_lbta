6a3b089cac0695d64c947793efc5fd983691873f
Lara_Bell <j8cwsqe29e2p@gmail.com>
2018-10-08 15:05:35 +0900

    Initial Commit

---
+++ autolayout_lbta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autolayout_lbta
+//
+//  Created by dev-mac on 2018/10/08.
+//  Copyright © 2018 lara-bell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
